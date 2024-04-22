e6151462301ea3bd4fab78a3cdc957dec30529eb
Rahul Barawal <Rahul.Barawal@bol7.com>
2024-04-22 14:25:13 +0530

    collectstatic

--- learning_fox/settings.py
+++ learning_fox/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-88g&v1u^1mv5-zd)$5bdn_u99-d+1syb&8&0t8tjtr^2y=pxvy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['learningfoxinteractive.com']
 
+CORS_ALLOWED_ORIGINS = ['https://learningfoxinteractive.com/', 'http://learningfoxinteractive.com/']
 
 # Application definition
 
@@ -42,13 +43,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'learning_fox.urls'
